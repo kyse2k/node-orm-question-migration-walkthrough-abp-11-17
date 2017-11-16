0fc1ff0bca3cf3c6b5e59e962d521731cdc6b0d5
Sérgio Pereira <sergiodanielpereira@gmail.com>
2017-11-16 09:54:17 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -4,8 +4,21 @@ const db = require("../config/db")
 
 class Question{
   static CreateTable(){
-    
+    const sql = `
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        age INTEGER
+      )
+    `
   }
+  console.log("Preparing to create the users table...")
+  return new Promise(function(resolve){
+      db.run(sql, function(){
+        console.log("...users table created!")
+        resolve("Success")        
+      })
+    })
 }
 
 
